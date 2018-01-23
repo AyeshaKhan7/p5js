73a28e2975d47be070d8caddf2e5fe178ddf21f8
Ayesha Khan <31661317+AyeshaKhan7@users.noreply.github.com>
2018-01-23 15:51:21 -0500

    uploading files

---
+++ empty-example/sketch.js
@@ -0,0 +1,53 @@
+//Ayesha Khan
+//1/9/18
+
+var bird;
+var pipes = [];
+var score;
+var lose;
+function setup () {
+  createCanvas(400, 600);
+  bird = new Bird();
+  score = new Score();
+  lose = new Lose();
+  pipes.push(new Pipe());
+}
+
+
+
+function draw(){
+  background(0);
+  score.c();
+  for (var i = pipes.length-1; i >=0; i--){
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)){
+      console.log("got it");
+      lose.page(score.cu());
+    }
+    else if (pipes[i].crosses(bird))
+    {
+          score.cu();
+    }
+
+    if (pipes[i].offscreen()){
+      pipes.splice(i, 1);
+    }
+  }
+
+  bird.update();
+  bird.show();
+
+  if (frameCount % 100 == 0){
+    pipes.push(new Pipe());
+  }
+
+}
+
+function keyPressed(){
+  if (key ==' '){
+    bird.up();
+    //console.log("SPACE");
+  }
+}
